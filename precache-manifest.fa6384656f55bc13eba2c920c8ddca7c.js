4f26426a1eedd8229686bd7d0b979cd0814e5cec
Nishant <nishant@Nishants-MacBook-Air.local>
2021-07-13 17:39:25 +0530

    Updates

---
+++ precache-manifest.fa6384656f55bc13eba2c920c8ddca7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6555995dfb118d00cb8",
+    "url": "/CryptoExchangeDapp/static/css/main.696e39df.chunk.css"
+  },
+  {
+    "revision": "d6555995dfb118d00cb8",
+    "url": "/CryptoExchangeDapp/static/js/main.d6555995.chunk.js"
+  },
+  {
+    "revision": "485deb770998bba004ed",
+    "url": "/CryptoExchangeDapp/static/css/1.b8a53443.chunk.css"
+  },
+  {
+    "revision": "485deb770998bba004ed",
+    "url": "/CryptoExchangeDapp/static/js/1.485deb77.chunk.js"
+  },
+  {
+    "revision": "3fc4e6fe425fddaf371e",
+    "url": "/CryptoExchangeDapp/static/js/runtime~main.3fc4e6fe.js"
+  },
+  {
+    "revision": "c06ccd6b67a3f32fbc2cfa138d1c07a6",
+    "url": "/CryptoExchangeDapp/static/media/SASILogo.c06ccd6b.png"
+  },
+  {
+    "revision": "bd758393e0e81722f83eccbf808c6d7a",
+    "url": "/CryptoExchangeDapp/static/media/ethLogo_.bd758393.png"
+  },
+  {
+    "revision": "a5e9c6c2941801a79ef6bf99e36c9903",
+    "url": "/CryptoExchangeDapp/index.html"
+  }
+];
