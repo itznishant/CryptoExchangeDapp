d20376c6956df0185f6456ea17507b3ce42e9ed7
System Administrator <root@Nishants-MacBook-Air.local>
2023-01-24 20:02:32 +0530

    compiles assets for deployment 24012022-2

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -1,5 +1,5 @@
 # rsync -r src/ docs/
 # rsync -r public/ docs/
-git add .
-git commit -am "compiles assets for deployment 24012022"
-git push
+# git add .
+# git commit -am "compiles assets for deployment 24012022"
+# git push
